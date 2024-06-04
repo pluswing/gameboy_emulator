8507a21983e2cd1b63d89bcd9dd2fc1fb116b4af
toshiyuki <toshiyuki.saito.16@gmail.com>
2024-06-05 07:52:20 +0900

    #064 タイルのキャッシュ処理を作成

--- src/cpu.rs
+++ src/cpu.rs
@@ -673,12 +673,14 @@ impl CPU {
 
 pub struct MemoryBus {
     memory: [u8; 0xFFFF],
+    gpu: GPU,
 }
 
 impl MemoryBus {
     fn new() -> Self {
         MemoryBus {
             memory: [0; 0xFFFF],
+            gpu: GPU::new(),
         }
     }
     pub fn read_byte(&self, address: u16) -> u8 {
@@ -726,6 +728,44 @@ fn empty_tile() -> Tile {
 struct GPU {
     vram: [u8; VRAM_SIZE],
     tile_set: [Tile; 384],
+}
+
+impl GPU {
+    pub fn new() -> Self {
+        GPU {
+            vram: [0; VRAM_SIZE],
+            tile_set: [empty_tile(); 384], // TODO これでいい？
+        }
+    }
+    pub fn read_vram(&self, address: usize) -> u8 {
+        self.vram[address]
+    }
+    pub fn write_vram(&mut self, index: usize, value: u8) {
+        self.vram[index] = value;
+
+        if index >= 0x1800 {
+            return;
+        }
+
+        let normalized_index = index & 0xFFFE;
+        let byte1 = self.vram[normalized_index];
+        let byte2 = self.vram[normalized_index + 1];
+        let tile_index = index / 16;
+        let row_index = (index % 16) / 2;
+
+        for pixel_index in 0..8 {
+            let mask = 1 << 7 - pixel_index;
+            let lsb = byte1 & mask;
+            let msb = byte2 & mask;
+            let value = match (lsb != 0, msb != 0) {
+                (true, true) => TilePixelValue::Three,
+                (false, true) => TilePixelValue::Two,
+                (true, false) => TilePixelValue::One,
+                (false, false) => TilePixelValue::Zero,
+            };
+            self.tile_set[tile_index][row_index][pixel_index] = value;
+        }
+    }
 }
 
 #[cfg(test)]
