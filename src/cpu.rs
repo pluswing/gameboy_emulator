36f3d2f434162f9c1cb4ccd3af4f44dd0267b99c
toshiyuki <toshiyuki.saito.16@gmail.com>
2024-06-10 10:13:11 +0900

    #067 fix code

--- src/cpu.rs
+++ src/cpu.rs
@@ -754,7 +754,7 @@ impl GPU {
         let row_index = (index % 16) / 2;
 
         for pixel_index in 0..8 {
-            let mask = 1 << 7 - pixel_index;
+            let mask = 1 << (7 - pixel_index);
             let lsb = byte1 & mask;
             let msb = byte2 & mask;
             let value = match (lsb != 0, msb != 0) {
