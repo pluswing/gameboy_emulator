67700c02be6c008ca8da384b9ae5d8e09c66332f
pluswing <toshiyuki@pluswing.co.jp>
2024-08-19 10:19:53 +0900

    #106 MBC1、大容量カートリッジへの対応。（仮）

--- src/mapper/mbc1.rs
+++ src/mapper/mbc1.rs
@@ -1,10 +1,18 @@
 pub struct MBC1 {
     bank: u8,
+    ram_enabled: u8,
+    extra: u8, // u2
+    mode: u8,  // u1
 }
 
 impl MBC1 {
     pub fn new() -> Self {
-        MBC1 { bank: 1 }
+        MBC1 {
+            bank: 1,
+            ram_enabled: 0,
+            extra: 0,
+            mode: 1,
+        }
     }
 
     pub fn read_byte(&mut self, raw: &Vec<u8>, addr: u16) -> u8 {
@@ -13,12 +21,16 @@ impl MBC1 {
             0x0000..=0x3FFF => raw[addr as usize],
             // bank1
             0x4000..=0x7FFF => {
-                let mut bank = self.bank & 0x1F;
+                let mut bank = ((self.extra & 0x02) << 5) | (self.bank & 0x1F);
                 if bank == 0 {
                     bank = 1
                 }
                 let addr = addr as usize + ((bank as usize - 1) * 0x4000);
                 raw[addr as usize]
+            }
+            0xA000..=0xBFFF => {
+                // TODO RAMアクセス
+                panic!("should not reach!")
             }
             _ => panic!("should not reach!"),
         }
@@ -31,15 +43,18 @@ impl MBC1 {
         match addr {
             0x0000..=0x1FFF => {
                 // TODO - RAM有効フラグ (W)
+                self.ram_enabled = value;
             }
             0x2000..=0x3FFF => {
                 self.bank = value;
             }
             0x400..=0x5FFF => {
                 // TODO RAMバンク番号 / ROMバンク番号の上位bit (W)
+                self.extra = value;
             }
             0x6000..=0x7FFF => {
                 // TODO バンクモードセレクト (W)
+                self.mode = value;
             }
             _ => panic!("should not reach!"),
         }
